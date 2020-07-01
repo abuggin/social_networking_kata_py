6b8f5b8d7d887871a11781c08803c7d3f57bde71
Ale <alessandrobuggin@gmail.com>
2020-07-01 13:08:00 +0100

    Red bar: Design posting message interaction

--- test_cli.py
+++ test_cli.py
@@ -10,6 +10,15 @@ class TestCliFeed(unittest.TestCase):
         actual = feed.get_usernames()
         self.assertEqual([username], actual)
 
+    def test_link_message_to_user(self):
+        feed = FeedCli()
+        username = "John"
+        message = "Hello!"
+        feed.add_user(username)
+        feed.post_message(username, message)
+        actual_messages = feed.get_messages_of(username)
+        self.assertEqual([message], actual_messages)
+
 
 if __name__ == "__main__":
     unittest.main()
