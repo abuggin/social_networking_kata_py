aef0eed27abdddb58276f5ef41c2b314c5d9b439
Ale <alessandrobuggin@gmail.com>
2020-07-02 14:17:16 +0100

    Red: Support write command

--- test_cli.py
+++ test_cli.py
@@ -12,6 +12,14 @@ class TestCliFeed(unittest.TestCase):
         cli.run(str_cmd)
         spy.get_messages_of.assert_called()
 
+    def test_post_message(self):
+        str_cmd = "Josh -> Hello!"
+        spy = mock.Mock(wraps=FeedCli())
+        cli = Cli(spy)
+        cli.run(str_cmd)
+        spy.post_message.assert_called()
+        spy.get_messages_of.assert_not_called()
+
 
 if __name__ == "__main__":
     unittest.main()
