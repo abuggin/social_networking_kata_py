6d070f56cee1a34fb98dcee4d986cebcf36c647e
Ale <alessandrobuggin@gmail.com>
2020-07-02 18:01:03 +0100

    Refactor tests to split parse vs run concerns

--- test_cli.py
+++ test_cli.py
@@ -6,43 +6,41 @@ from unittest import mock
 
 
 class TestCliFeed(unittest.TestCase):
+    josh_hello_cmd = Command(
+        action=Action.WRITE, actor="Josh", arg_dependant_on_action="Hello!"
+    )
+    jonny_display_cmd = Command(
+        action=Action.DISPLAY_OWN_POSTS, actor="Jonny", arg_dependant_on_action=None
+    )
+
     def test_display_command(self):
-        str_cmd = "Josh"
         spy = mock.Mock(wraps=FeedCli())
         cli = Cli(spy)
-        cli.run(cli.parse(str_cmd))
+        cli.run(self.jonny_display_cmd)
         spy.get_messages_of.assert_called()
 
     def test_post_message(self):
-        str_cmd = "Josh -> Hello!"
         spy = mock.Mock(wraps=FeedCli())
         cli = Cli(spy)
-        cli.run(cli.parse(str_cmd))
+        cli.run(self.josh_hello_cmd)
         spy.post_message.assert_called_once_with(username="Josh", message="Hello!")
         spy.get_messages_of.assert_not_called()
 
     def test_parse_command(self):
         cli = Cli(None)
         res = cli.parse("Josh -> Hello!")
-        parsed_cmd = Command(
-            action=Action.WRITE, actor="Josh", arg_dependant_on_action="Hello!"
-        )
+        parsed_cmd = self.josh_hello_cmd
         self.assertEqual(parsed_cmd, res)
 
     def test_hash_command(self):
-        cmd = Command(
+        another_josh_hello_cmd = Command(
             action=Action.WRITE, actor="Josh", arg_dependant_on_action="Hello!"
         )
-        cmd_1 = Command(
-            action=Action.WRITE, actor="Josh", arg_dependant_on_action="Hello!"
-        )
-        self.assertEqual(cmd, cmd_1)
-        self.assertEqual(hash(cmd), hash(cmd_1))
+        self.assertEqual(another_josh_hello_cmd, self.josh_hello_cmd)
+        self.assertEqual(hash(another_josh_hello_cmd), hash(self.josh_hello_cmd))
 
     def test_parse_display_command(self):
-        parsed_cmd = Command(
-            action=Action.DISPLAY_OWN_POSTS, actor="Jonny", arg_dependant_on_action=None
-        )
+        parsed_cmd = self.jonny_display_cmd
         self.assertEqual(parsed_cmd, Cli(None).parse("Jonny"))
 
 
